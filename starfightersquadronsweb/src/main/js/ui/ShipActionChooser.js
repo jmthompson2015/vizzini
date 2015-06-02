b90f7057bb8bc681b6c6eff1b757268f2e1503ef
jmthompson2015 <jmthompson2007@gmail.com>
2015-06-02 08:53:12 -0600

    Changed to a React implementation.

--- starfightersquadronsweb/src/main/js/ui/ShipActionChooser.js
+++ starfightersquadronsweb/src/main/js/ui/ShipActionChooser.js
@@ -1,106 +1,53 @@
 /*
  * Provides a user interface to choose a ship action.
  */
-function ShipActionChooser(environment, attacker, shipActions)
+var ShipActionChooser = React.createClass(
 {
-    this.okActionPerformed = function()
+    getInitialState: function() 
     {
-        var answer = getShipAction();
-
-        callback(answer);
-    }
-
-    this.paintComponent = function()
+        return {selectedIndex: 0};
+    },
+    
+    render: function() 
     {
-        var answer = "";
-
-        answer += "<table class='shipActionPanel'>";
-
-        answer += "<tr>";
-        answer += "<td class='shipActionPanel' colspan='2'>";
-        answer += "Select Action";
-        answer += "</td>";
-        answer += "</tr>";
-
-        if (attacker)
-        {
-            answer += "<tr>";
-            answer += "<td colspan='2'>";
-            answer += "Active Ship: ";
-            answer += attacker.getName();
-            answer += "</td>";
-            answer += "</tr>";
-        }
-
-        answer += "<tr>";
-        answer += "<td>";
-        answer += createShipActionTable();
-        answer += "</td>";
-
-        answer += "<td class='shipActionOk'>";
-        answer += "<button type='button' onclick='ShipActionChooser.instance.okActionPerformed()'>OK</button>";
-        answer += "</td>";
-
-        answer += "</tr>";
-        answer += "</table>";
-
-        return answer;
-    }
-
-    function createShipActionTable()
-    {
-        var answer = "";
-
-        answer += "<table id='shipActionTable'>";
-
-        for (var i = 0; i < shipActions.length; i++)
-        {
-            var shipAction = shipActions[i];
-
-            answer += "<tr>";
-            answer += "<td class='shipActionCell'>";
-            answer += "<input type='radio' name='shipActionRadio' value=";
-            answer += i;
-            answer += ">";
-            answer += ShipAction.properties[shipAction].displayName;
-            answer += "</input>";
-            answer += "</td>";
-            answer += "</tr>";
-        }
-
-        answer += "</table>";
-
-        return answer;
-    }
-
-    function getShipAction()
-    {
-        // HTMLTableElement
-        var element = document.getElementById("shipActionTable");
-
-        var answer;
-
-        // Walk the table rows to find the selected element.
-        var rows = element.rows;
-
-        for (var i = 0; i < rows.length; i++)
-        {
-            // HTMLTableRowElement
-            var row = rows[i];
-            // HTMLTableCellElement
-            var cell = row.cells[0];
-            // HTMLInputElement
-            var radio = cell.firstElementChild;
-
-            if (radio.checked)
+        var token = this.props.token;
+        var message = "Active Ship: " + token.getName();
+        var shipActions = this.props.shipActions;
+        var options = shipActions.map(function(shipAction)
             {
-                answer = shipActions[radio.value];
-                break;
+                return ShipAction.properties[shipAction].displayName;
             }
-        }
+        );
+        var self = this;
+        
+        return (<OptionPane panelClass="optionPane"
+            title="Select Action" titleClass="optionPaneTitle"
+            message={message} messageClass="optionPaneMessage"
+            initialInput={<RadioButtonList panelClass="optionPaneInput" name="shipActionRadio" options={options} onChange={self.selectionChanged} />}
+            buttons={<span><button onClick={self.cancel}>Cancel</button>
+                <button onClick={self.ok}>OK</button></span>}
+            buttonsClass="optionPaneButtons"
+        />);
+    },
 
-        return answer;
-    }
+    selectionChanged: function(event)
+    {
+        var index = event.currentTarget.value;
+        LOGGER.debug("selectionChanged() selectedIndex = " + index);
+        this.setState({selectedIndex: index});
+    },
 
-    ShipActionChooser.instance = this;
-}
+    cancel: function()
+    {
+        LOGGER.debug("Cancel clicked");
+        this.props.callback(undefined);
+    },
+
+    ok: function()
+    {
+        var selectedIndex = this.state.selectedIndex;
+        var selectedShipAction = this.props.shipActions[selectedIndex];
+        LOGGER.debug("OK clicked, selectedIndex = " + selectedIndex+ " "+selectedShipAction);
+        this.props.callback(selectedShipAction);
+    },
+});
