2b86526078550cacd503a19378a3979e8a58632a
jmthompson2015 <jmthompson2007@gmail.com>
2015-06-11 13:37:43 -0600

    Initial version.

---
+++ aiweb/src/main/js/geneticalgorithm/GenomeFactory.js
@@ -0,0 +1,23 @@
+// require("InputValidator");
+// require("ArrayAugments");
+
+/*
+ * Provides a factory to create a genome.
+ */
+function GenomeFactory(genes, genomeLength)
+{
+    InputValidator.validateNotEmpty("genes", genes);
+    InputValidator.validateIsNumber("genomeLength", genomeLength);
+
+    this.create = function()
+    {
+        var answer = [];
+
+        for (var i = 0; i < genomeLength; i++)
+        {
+            answer[i] = Array.Vizzini.randomElement(genes);
+        }
+
+        return answer;
+    }
+}
