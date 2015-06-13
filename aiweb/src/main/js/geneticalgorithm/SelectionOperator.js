fef23c7fccca19117697eed2d99d7c065473ed4d
jmthompson2015 <jmthompson2007@gmail.com>
2015-06-13 10:11:03 -0600

    Added a tournamentSelect() function.

--- aiweb/src/main/js/geneticalgorithm/SelectionOperator.js
+++ aiweb/src/main/js/geneticalgorithm/SelectionOperator.js
@@ -17,6 +17,21 @@ var SelectionOperator =
 
         return Array.Vizzini.randomElement(head);
     },
+
+    tournamentSelect: function(selectionCount, population)
+    {
+        InputValidator.validateNotNull("population", population);
+        InputValidator.validateInRange("selectionCount", selectionCount, 0,
+                population.length);
+
+        // This assumes population is sorted.
+        var head = population.slice(0, selectionCount);
+
+        var genome0 = Array.Vizzini.randomElement(head);
+        var genome1 = Array.Vizzini.randomElement(head);
+
+        return (genome0.fitness > genome1.fitness ? genome0 : genome1);
+    },
 }
 
 function Selector(selectionCount, selectFunction)
