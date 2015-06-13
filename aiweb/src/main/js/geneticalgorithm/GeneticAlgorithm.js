2daae597b1fdd2e685ca55fcb1b722889168f8ff
jmthompson2015 <jmthompson2007@gmail.com>
2015-06-13 10:01:15 -0600

    Code cleanup.

--- aiweb/src/main/js/geneticalgorithm/GeneticAlgorithm.js
+++ aiweb/src/main/js/geneticalgorithm/GeneticAlgorithm.js
@@ -30,14 +30,94 @@ function GeneticAlgorithm(populationIn, evaluator, generationCount, comparator,
             - crossoverCount);
     var bestEvals = [];
 
-    this.getPopulation = function()
+    /**
+     * Create the next generation's population.
+     */
+    this.createNextGeneration = function()
     {
-        return population;
+        LOGGER.trace("GeneticAlgorithm.createNextGeneration() start");
+        var newPop = [];
+
+        // Copy over the first X genomes unchanged.
+        for (var i = 0; i < copyCount; i++)
+        {
+            newPop[newPop.length] = population[i].slice();
+        }
+
+        // Crossover the top X genomes.
+        var maxTries = 100;
+        this.fillPopulation(newPop, (copyCount + crossoverCount), maxTries,
+                true);
+
+        // Mutate the top X genomes.
+        this.fillPopulation(newPop, population.length, maxTries, false);
+
+        population = newPop.slice();
+        LOGGER.trace("GeneticAlgorithm.createNextGeneration() start");
     }
 
     this.determineBest = function(callback)
     {
         this.runGeneration(0, callback);
+    }
+
+    this.fillPopulation = function(newPop, popSize, maxTries, isCrossover)
+    {
+        var count = 0;
+
+        while (newPop.length < popSize)
+        {
+            var genome = null;
+
+            if (count < maxTries)
+            {
+                var genome1 = selector.select(population);
+
+                if (isCrossover)
+                {
+                    var genome2 = selector.select(population);
+                    genome = crossoverOperator(genome1, genome2);
+                }
+                else
+                {
+                    genome = mutator.mutate(genome1);
+                }
+            }
+            else
+            {
+                genome = genomeFactory.create();
+            }
+
+            if (genome === undefined || genome === null)
+            {
+                LOGGER.error("ERROR: genome is undefined or null");
+                throw "ERROR: genome is undefined or null";
+            }
+
+            if (!Array.Vizzini.containsUsingEquals(newPop, genome,
+                    Array.Vizzini.equals))
+            {
+                newPop[newPop.length] = genome;
+                count = 0;
+            }
+
+            count++;
+        }
+    }
+
+    this.fireGenerationCompleted = function(generationCount)
+    {
+        this.trigger("generation", generationCount);
+    }
+
+    this.fireMessage = function(message)
+    {
+        this.trigger("message", message);
+    }
+
+    this.getPopulation = function()
+    {
+        return population;
     }
 
     this.runGeneration = function(g, callback)
@@ -95,86 +175,6 @@ function GeneticAlgorithm(populationIn, evaluator, generationCount, comparator,
                 self.runGeneration(g + 1, callback);
             }, 300);
         }
-    }
-
-    /**
-     * Create the next generation's population.
-     */
-    this.createNextGeneration = function()
-    {
-        LOGGER.trace("GeneticAlgorithm.createNextGeneration() start");
-        var newPop = [];
-
-        // Copy over the first X genomes unchanged.
-        for (var i = 0; i < copyCount; i++)
-        {
-            newPop[newPop.length] = population[i].slice();
-        }
-
-        // Crossover the top X genomes.
-        var maxTries = 100;
-        this.fillPopulation(newPop, (copyCount + crossoverCount), maxTries,
-                true);
-
-        // Mutate the top X genomes.
-        this.fillPopulation(newPop, population.length, maxTries, false);
-
-        population = newPop.slice();
-        LOGGER.trace("GeneticAlgorithm.createNextGeneration() start");
-    }
-
-    this.fillPopulation = function(newPop, popSize, maxTries, isCrossover)
-    {
-        var count = 0;
-
-        while (newPop.length < popSize)
-        {
-            var genome = null;
-
-            if (count < maxTries)
-            {
-                var genome1 = selector.select(population);
-
-                if (isCrossover)
-                {
-                    var genome2 = selector.select(population);
-                    genome = crossoverOperator(genome1, genome2);
-                }
-                else
-                {
-                    genome = mutator.mutate(genome1);
-                }
-            }
-            else
-            {
-                genome = genomeFactory.create();
-            }
-
-            if (genome === undefined || genome === null)
-            {
-                LOGGER.error("ERROR: genome is undefined or null");
-                throw "ERROR: genome is undefined or null";
-            }
-
-            if (!Array.Vizzini.containsUsingEquals(newPop, genome,
-                    Array.Vizzini.equals))
-            {
-                newPop[newPop.length] = genome;
-                count = 0;
-            }
-
-            count++;
-        }
-    }
-
-    this.fireGenerationCompleted = function(generationCount)
-    {
-        this.trigger("generation", generationCount);
-    }
-
-    this.fireMessage = function(message)
-    {
-        this.trigger("message", message);
     }
 }
 
