4dfe166bd343e4db53ce82a1f3e57653681120b5
jmthompson2015 <jmthompson2007@gmail.com>
2015-07-01 20:36:22 -0600

    Initial version.

---
+++ starfightersquadronsweb/src/main/js/ui/SquadBuilderUI.js
@@ -0,0 +1,147 @@
+/*
+ * Provides a user interface to build a squad.
+ * 
+ * @param initialTeam Initial team.
+ */
+var SquadBuilderUI = React.createClass(
+{
+    getInitialState: function()
+    {
+        var team = this.props.team;
+        
+        // Default to first ship, first pilot.
+        var ship = Ship.valuesByTeam(team)[0];
+        var pilot = Pilot.valuesByShip(ship)[0];
+        var token = this.createToken(team, pilot);
+
+        return {pilot: pilot, token: token, upgrades: [], squad: []};
+    },
+    
+    componentDidMount: function() 
+    {  
+        this.renderSquadUI();
+    },
+    
+    render: function() 
+    {
+        var team = this.props.team;
+        var upgradesUI = this.createUpgradesUI();
+        var addButton = <input type="button" value="Add" onClick={this.addActionPerformed} />
+        
+        var rows = [];
+        var rowCount = 0;
+        
+        rows[rows.length] = <tr key={rowCount}><td><PilotChooser team={team} onChangeFunction={this.pilotChanged} /></td></tr>;
+        rowCount++;
+        rows[rows.length] = <tr key={rowCount}><td>{upgradesUI}</td></tr>;
+        rowCount++;
+        rows[rows.length] = <tr key={rowCount}><td>{addButton}</td></tr>;
+        rowCount++;
+        rows[rows.length] = <tr key={rowCount}><td id="squadPanel"></td></tr>;
+        rowCount++;
+        
+        return <table className="squadBuilderUI">{rows}</table>;
+    },
+    
+    componentDidUpdate: function()
+    {
+        LOGGER.trace("SquadBuilderUI.componentDidUpdate()");
+        this.renderSquadUI();
+    },
+
+    pilotChanged: function(event, pilot)
+    {
+        LOGGER.debug("new pilot = " + pilot);
+        var team = this.props.team;
+        var token = this.createToken(team, pilot);
+        this.setState({pilot: pilot, token: token, upgrades: []});
+    },
+    
+    upgradeChanged: function(event)
+    {
+        var upgradeCard = event.currentTarget.value;
+        LOGGER.debug("new upgradeCard = " + upgradeCard);
+        var index = event.currentTarget.dataset.index;
+        var upgrades = this.state.upgrades;
+        
+        upgrades[index] = (upgradeCard === "*none*") ? undefined : upgradeCard;
+        this.setState({upgrades: upgrades});
+    },
+    
+    addActionPerformed: function(event)
+    {
+        LOGGER.trace("add clicked");
+        var token = this.createToken(this.props.team, this.state.pilot);
+        
+        // Add upgrade cards.
+        var myUgrades = this.state.upgrades;
+        var tokenUpgrades = token.getUpgrades();
+        
+        for (var i=0; i<myUgrades.length; i++)
+        {
+            var upgrade = myUgrades[i];
+            
+            if (upgrade)
+            {
+                tokenUpgrades[tokenUpgrades.length] = upgrade;
+            }
+        }
+        
+        var squad = this.state.squad;
+        squad[squad.length] = token;
+        this.setState({squad: squad});
+    },
+
+    removeActionPerformed: function(selected, event)
+    {
+        LOGGER.trace("remove clicked");
+        var squad = this.state.squad;
+
+        for (var i=0; i<selected.length; i++)
+        {
+            var token = selected[i];
+            var index = squad.indexOf(token);
+
+            if (index >= 0)
+            {
+                squad.splice(index, 1);
+            }
+        }
+
+        this.renderSquadUI();
+    },
+
+    renderSquadUI: function()
+    {
+        var squad = this.state.squad;
+        
+        React.render(<SquadUI squad={squad} removeFunction={this.removeActionPerformed} />, 
+            document.getElementById("squadPanel"));
+    },
+
+    createToken: function(team, pilot)
+    {
+        var agentName = (team === Team.IMPERIAL) ? "Imperial Agent" : "Rebel Agent";
+        var squadBuilder = (team === Team.IMPERIAL) ? CoreSetImperialSquadBuilder : CoreSetRebelSquadBuilder;
+        var agent = new SimpleAgent(agentName, team, squadBuilder);
+        
+        return new Token(pilot, agent);
+    },
+
+    createUpgradesUI: function()
+    {
+        var pilot = this.state.pilot;
+        var upgradeTypes = Pilot.properties[pilot].upgradeTypes;
+        var columns = [];
+        
+        for (var i=0; i<upgradeTypes.length; i++)
+        {
+            var upgradeType = upgradeTypes[i];
+            columns[columns.length] = <td key={i}><UpgradeChooser pilot={pilot} upgradeType={upgradeType} index={i} onChangeFunction={this.upgradeChanged}/></td>;
+        }
+        
+        return <table className="squadBuilderUpgradesUI">
+            <tbody><tr>{columns}</tr></tbody>
+            </table>;
+    },
+});
---
+++ starfightersquadronsweb/src/main/js/ui/SquadUI.js
@@ -0,0 +1,161 @@
+/*
+ * Provides a user interface for a starfighter squadron.
+ * 
+ * @param squad Squad.
+ * @param removeFunction Called after an item is removed.
+ */
+var SquadUI = React.createClass(
+{
+    getInitialState: function()
+    {
+        return {selected: []};
+    },
+    
+    render: function() 
+    {
+        var that = this;
+        var checkAll = <input type="checkbox" onChange={this.selectAllActionPerformed} />;
+        
+        var rows = [];
+        
+        var header = <tr key={-1}>
+            <th>{checkAll}</th>
+            <th>Pilot</th>
+            <th>Ship</th>
+            <th>Pilot Skill</th>
+            <th>Primary Weapon</th>
+            <th>Agility</th>
+            <th>Hull</th>
+            <th>Shield</th>
+            <th>Squad Points</th>
+            </tr>;
+            
+        var squad = this.props.squad;
+        var pilotSkillSum = 0;
+        var primaryWeaponSum = 0;
+        var agilitySum = 0;
+        var hullSum = 0;
+        var shieldSum = 0;
+        var squadPointsSum = 0;
+
+        for (var i=0; i<squad.length; i++)
+        {
+            var token = squad[i];
+            var isChecked = Array.Vizzini.contains(this.state.selected, token);
+            var pilot = token.getPilot();
+            var pilotProps = Pilot.properties[pilot];
+            var shipState = pilotProps.shipState;
+
+            pilotSkillSum += shipState.getPilotSkillValue();
+            primaryWeaponSum += shipState.getPrimaryWeaponValue();
+            agilitySum += shipState.getAgilityValue();
+            hullSum += shipState.getHullValue();
+            shieldSum += shipState.getShieldValue();
+            squadPointsSum += pilotProps.squadPointCost;
+            
+            rows[rows.length] = <tr key={token.getId()}>
+                <td><input type="checkbox" checked={isChecked} onChange={that.setSelected.bind(this, token)} /></td>
+                <td className="squadUIPilotName">{pilotProps.name}</td>
+                <td className="squadUIPilotName">{Ship.properties[pilotProps.ship].name}</td>
+                <td>{shipState.getPilotSkillValue()}</td>
+                <td>{shipState.getPrimaryWeaponValue()}</td>
+                <td>{shipState.getAgilityValue()}</td>
+                <td>{shipState.getHullValue()}</td>
+                <td>{shipState.getShieldValue()}</td>
+                <td>{pilotProps.squadPointCost}</td>
+                </tr>;
+                
+            var upgrades = token.getUpgrades();
+            
+            if (upgrades.length > 0)
+            {
+                for (var j=0; j<upgrades.length; j++)
+                {
+                    var upgrade = upgrades[j];
+                    var upgradeProps = UpgradeCard.properties[upgrade];
+                    
+                    squadPointsSum += upgradeProps.squadPointCost;
+
+                    rows[rows.length] = <tr key={(100 * token.getId()) + j}>
+                        <td>&nbsp;</td>
+                        <td className="squadUIPilotName">
+                            {UpgradeCardUI.createUpgradeImage(upgradeProps.type)} {upgradeProps.name}</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>{upgradeProps.squadPointCost}</td>
+                        </tr>;
+                }
+            }
+        }
+        
+        rows[rows.length] = <tr key={2000}>
+            <td className="squadUISum">&nbsp;</td>
+            <td className="squadUISum">Totals</td>
+            <td className="squadUISum">&nbsp;</td>
+            <td className="squadUISum">{pilotSkillSum}</td>
+            <td className="squadUISum">{primaryWeaponSum}</td>
+            <td className="squadUISum">{agilitySum}</td>
+            <td className="squadUISum">{hullSum}</td>
+            <td className="squadUISum">{shieldSum}</td>
+            <td className="squadUISum">{squadPointsSum}</td>
+            </tr>;
+            
+        var removeButton = <input type="button" value="Remove" onClick={this.removeFunction.bind(this, this.state.selected)} />;
+
+        return <table>
+            <tbody>
+            <tr>
+            <td>
+                <table className="squadUI"><thead>{header}</thead><tbody>{rows}</tbody></table>
+            </td>
+            </tr>
+            <tr>
+                <td>{removeButton}</td>
+            </tr>
+            </tbody>
+            </table>;
+    },
+
+    selectAllActionPerformed: function(event)
+    {
+        var selected = [];
+        var squad = this.props.squad;
+        
+        if (event.target.checked)
+        {
+            selected = squad.slice();
+        }
+        
+        this.setState({selected: selected});
+    },
+    
+    setSelected: function(token, event)
+    {
+        var selected = this.state.selected;
+
+        if (event.target.checked)
+        {
+            if (!Array.Vizzini.contains(selected, token))
+            {
+                selected[selected.length] = token;
+            }
+        }
+        else
+        {
+            var index = selected.indexOf(token);
+            selected.splice(index, 1);
+        }
+        
+        this.setState({selected: selected});
+    },
+    
+    removeFunction: function(selected, event)
+    {
+        this.props.removeFunction(selected, event);
+        this.setState({selected: []});
+    },
+});
