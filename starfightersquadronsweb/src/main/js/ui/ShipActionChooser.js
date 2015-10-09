6247dbfa120a09a150cef4eeb18f0dd9509f0312
jmthompson2015 <jmthompson2007@gmail.com>
2015-10-09 11:40:46 -0600

    Removed use of JSX in ShipActionChooser.

--- starfightersquadronsweb/src/main/js/ui/ShipActionChooser.js
+++ starfightersquadronsweb/src/main/js/ui/ShipActionChooser.js
@@ -3,38 +3,64 @@
  */
 var ShipActionChooser = React.createClass(
 {
-    getInitialState: function() 
+    getInitialState: function()
     {
-        return {selectedIndex: 0};
+        return (
+        {
+            selectedIndex: 0
+        });
     },
-    
-    render: function() 
+
+    render: function()
     {
         var token = this.props.token;
         var message = "Active Ship: " + token.getName();
         var shipActions = this.props.shipActions;
         var options = shipActions.map(function(shipAction)
-            {
-                return ShipAction.properties[shipAction].displayName;
-            }
-        );
+        {
+            return ShipAction.properties[shipAction].displayName;
+        });
         var self = this;
-        
-        return (<OptionPane panelClass="optionPane"
-            title="Select Action" titleClass="optionPaneTitle"
-            message={message} messageClass="optionPaneMessage"
-            initialInput={<RadioButtonList panelClass="optionPaneInput" name="shipActionRadio" options={options} onChange={self.selectionChanged} />}
-            buttons={<span><button onClick={self.cancel}>Cancel</button>
-                <button onClick={self.ok}>OK</button></span>}
-            buttonsClass="optionPaneButtons"
-        />);
+        var initialInput = React.createElement(RadioButtonList,
+        {
+            panelClass: "optionPaneInput",
+            name: "shipActionRadio",
+            options: options,
+            onChange: self.selectionChanged
+        });
+        var cancelButton = React.DOM.button(
+        {
+            key: 0,
+            onClick: self.cancel
+        }, "Cancel");
+        var okButton = React.DOM.button(
+        {
+            key: 1,
+            onClick: self.ok
+        }, "OK");
+        var buttons = React.DOM.span({}, [ cancelButton, okButton ]);
+
+        return React.createElement(OptionPane,
+        {
+            panelClass: "optionPane",
+            title: "Select Action",
+            titleClass: "optionPaneTitle",
+            message: message,
+            messageClass: "optionPaneMessage",
+            initialInput: initialInput,
+            buttons: buttons,
+            buttonsClass: "optionPaneButtons"
+        });
     },
 
     selectionChanged: function(event)
     {
         var index = event.currentTarget.value;
         LOGGER.debug("selectionChanged() selectedIndex = " + index);
-        this.setState({selectedIndex: index});
+        this.setState(
+        {
+            selectedIndex: index
+        });
     },
 
     cancel: function()
@@ -47,7 +73,8 @@ var ShipActionChooser = React.createClass(
     {
         var selectedIndex = this.state.selectedIndex;
         var selectedShipAction = this.props.shipActions[selectedIndex];
-        LOGGER.debug("OK clicked, selectedIndex = " + selectedIndex+ " "+selectedShipAction);
+        LOGGER.debug("OK clicked, selectedIndex = " + selectedIndex + " "
+                + selectedShipAction);
         this.props.callback(selectedShipAction);
     },
 });
