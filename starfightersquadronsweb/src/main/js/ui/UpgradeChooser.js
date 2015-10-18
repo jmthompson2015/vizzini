cfa1fb86a9cca74a895a698831feb1b10f069d51
jmthompson2015 <jmthompson2007@gmail.com>
2015-10-18 11:56:12 -0600

    Changed to display the squad point cost in the select option.

--- starfightersquadronsweb/src/main/js/ui/UpgradeChooser.js
+++ starfightersquadronsweb/src/main/js/ui/UpgradeChooser.js
@@ -6,107 +6,106 @@
  * @param index Index of this chooser.
  * @param onChangeFunction Called for the select onChange event.
  */
-var UpgradeChooser = React
-        .createClass(
+var UpgradeChooser = React.createClass(
+{
+    getInitialState: function()
+    {
+        return (
         {
-            getInitialState: function()
-            {
-                return (
-                {
-                    selected: undefined
-                });
-            },
-
-            render: function()
-            {
-                var pilot = this.props.pilot;
-                var upgradeType = this.props.upgradeType;
-                var upgrades = UpgradeCard.valuesByPilotAndType(pilot,
-                        upgradeType);
-                upgrades.unshift("*none*");
-
-                var rows = [];
-
-                var image = UpgradeCardUI.createUpgradeImage(upgradeType, 0);
-                var labelFunction = function(value)
-                {
-                    return (UpgradeCard.properties[value] ? UpgradeCard.properties[value].name
-                            : value);
-                };
-                var select = React.createElement(Select,
-                {
-                    key: 2,
-                    values: upgrades,
-                    labelFunction: labelFunction,
-                    onChange: this.upgradeCardChanged,
-                    clientProps:
-                    {
-                        "data-index": this.props.index
-                    }
-                });
-
-                rows.push(React.DOM.tr(
-                {
-                    key: 0
-                }, React.DOM.td({}, image, select)));
-
-                rows.push(React.DOM.tr(
-                {
-                    key: 1
-                }, React.DOM.td(
-                {
-                    id: this.createId()
-                }, " ")));
-
-                var tbody = React.DOM.tbody({}, rows);
-
-                return React.DOM.table(
-                {
-                    className: "upgradeChooser"
-                }, tbody);
-            },
-
-            createId: function()
-            {
-                var pilot = this.props.pilot;
-                var upgradeType = this.props.upgradeType;
-                var index = this.props.index;
-
-                return pilot + upgradeType + index;
-            },
-
-            getSelected: function()
-            {
-                return this.state.selected;
-            },
-
-            upgradeCardChanged: function(event)
-            {
-                var upgradeCard = event.currentTarget.value;
-                var id = this.createId();
-                var element = document.getElementById(id);
-
-                if (upgradeCard === "*none*")
-                {
-                    this.setState(
-                    {
-                        selected: undefined
-                    });
-                    element.innerHTML = "";
-                }
-                else
-                {
-                    this.setState(
-                    {
-                        selected: upgradeCard
-                    });
-                    var component = React.createElement(UpgradeCardUI,
-                    {
-                        upgradeCard: upgradeCard
-                    });
-                    React.render(component, element);
-                }
-
-                this.props.onChangeFunction(event);
-            },
+            selected: undefined
         });
+    },
+
+    render: function()
+    {
+        var pilot = this.props.pilot;
+        var upgradeType = this.props.upgradeType;
+        var upgrades = UpgradeCard.valuesByPilotAndType(pilot, upgradeType);
+        upgrades.unshift("*none*");
+
+        var rows = [];
+
+        var image = UpgradeCardUI.createUpgradeImage(upgradeType, 0);
+        var labelFunction = function(value)
+        {
+            var upgradeProps = UpgradeCard.properties[value];
+            return (upgradeProps ? upgradeProps.name + " "
+                    + upgradeProps.squadPointCost : value);
+        };
+        var select = React.createElement(Select,
+        {
+            key: 2,
+            values: upgrades,
+            labelFunction: labelFunction,
+            onChange: this.upgradeCardChanged,
+            clientProps:
+            {
+                "data-index": this.props.index
+            }
+        });
+
+        rows.push(React.DOM.tr(
+        {
+            key: 0
+        }, React.DOM.td({}, image, select)));
+
+        rows.push(React.DOM.tr(
+        {
+            key: 1
+        }, React.DOM.td(
+        {
+            id: this.createId()
+        }, " ")));
+
+        var tbody = React.DOM.tbody({}, rows);
+
+        return React.DOM.table(
+        {
+            className: "upgradeChooser"
+        }, tbody);
+    },
+
+    createId: function()
+    {
+        var pilot = this.props.pilot;
+        var upgradeType = this.props.upgradeType;
+        var index = this.props.index;
+
+        return pilot + upgradeType + index;
+    },
+
+    getSelected: function()
+    {
+        return this.state.selected;
+    },
+
+    upgradeCardChanged: function(event)
+    {
+        var upgradeCard = event.currentTarget.value;
+        var id = this.createId();
+        var element = document.getElementById(id);
+
+        if (upgradeCard === "*none*")
+        {
+            this.setState(
+            {
+                selected: undefined
+            });
+            element.innerHTML = "";
+        }
+        else
+        {
+            this.setState(
+            {
+                selected: upgradeCard
+            });
+            var component = React.createElement(UpgradeCardUI,
+            {
+                upgradeCard: upgradeCard
+            });
+            React.render(component, element);
+        }
+
+        this.props.onChangeFunction(event);
+    },
+});
