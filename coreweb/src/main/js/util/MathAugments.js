297c81f999261e9f9a8b33932337926805679ee4
jmthompson2015 <jmthompson2007@gmail.com>
2015-10-22 10:46:50 -0600

    Added vizziniFormat() and vizziniRound() functions.

--- coreweb/src/main/js/util/MathAugments.js
+++ coreweb/src/main/js/util/MathAugments.js
@@ -2,6 +2,22 @@
  * Provides utility methods for Math.
  */
 
+/*
+ * @param number The number to format.
+ * @param digits The number of digits to appear after the decimal point. (optional)
+ */
+Math.vizziniFormat = function(number, digits)
+{
+    var answer = null;
+
+    if (number && !isNaN(number))
+    {
+        answer = number.toFixed(digits);
+    }
+
+    return answer;
+}
+
 Math.vizziniRandomIntFromRange = function(min, max)
 {
     var value = Math.vizziniRandomRealFromRange(min, max);
@@ -14,8 +30,21 @@ Math.vizziniRandomRealFromRange = function(min, max)
     return Math.random() * (max - min) + min;
 }
 
+/*
+ * @param number The number to round.
+ * 
+ * @param digits The number of digits to appear after the decimal point.
+ */
+Math.vizziniRound = function(number, digits)
+{
+    var factor = Math.pow(10.0, digits);
+
+    return Math.round(factor * number) / factor;
+}
+
 Math.vizziniRound2 = function(value)
 {
+    console.warn("Deprecated: Math.vizziniRound2(value); use Math.vizziniRound(number, digits) instead.");
     var factor = 100.0;
 
     return Math.round(factor * value) / factor;
@@ -23,6 +52,7 @@ Math.vizziniRound2 = function(value)
 
 Math.vizziniRound4 = function(value)
 {
+    console.warn("Deprecated: Math.vizziniRound4(value); use Math.vizziniRound(number, digits) instead.");
     var factor = 10000.0;
 
     return Math.round(factor * value) / factor;
