948225815e61607fa529129bf287d992ad053c1d
jmthompson <jmthompson@192.168.1.9>
2015-12-04 13:20:17 -0700

    Initial version.

---
+++ starfightersquadronsweb/src/main/js/MediumAgent.js
@@ -0,0 +1,139 @@
+/*
+ * Provides a medium implementation of a computer agent for Starfighter Squadrons.
+ */
+function MediumAgent(name, team, squadBuilder)
+{
+    InputValidator.validateNotEmpty("name", name);
+    InputValidator.validateNotNull("team", team);
+    InputValidator.validateNotNull("squadBuilder", squadBuilder);
+
+    this.getName = function()
+    {
+        return name;
+    }
+
+    this.getSquadBuilder = function()
+    {
+        return squadBuilder;
+    }
+
+    this.getTeam = function()
+    {
+        return team;
+    }
+}
+
+MediumAgent.prototype = SimpleAgent.prototype;
+
+/*
+ * @param environment Environment.
+ * 
+ * @param adjudicator Adjudicator.
+ * 
+ * @return a new action.
+ */
+MediumAgent.prototype.getPlanningAction = function(environment, adjudicator, callback)
+{
+    InputValidator.validateNotNull("environment", environment);
+    InputValidator.validateNotNull("adjudicator", adjudicator);
+    InputValidator.validateNotNull("callback", callback);
+
+    var team = this.getTeam();
+    var tokens = environment.getTokensForTeam(team);
+    var defenders = environment.getDefenders(team);
+    var rangeRuler = new RangeRuler();
+    var tokenToManeuver = {};
+
+    tokens.forEach(function(token)
+    {
+        var fromPosition = environment.getPositionFor(token);
+        var shipBase = token.getShipBase();
+        var maneuvers = token.getManeuvers();
+        LOGGER.trace("maneuvers.length = " + maneuvers.length + " for " + token);
+
+        // Find the maneuvers which keep the ship on the battlefield.
+        var validManeuvers = [];
+
+        // Find the maneuvers which take the ship within range of a defender.
+        var validManeuversR1 = [];
+        var validManeuversR2 = [];
+        var validManeuversR3 = [];
+
+        maneuvers.forEach(function(maneuver)
+        {
+            var toPosition = Maneuver.computeToPosition(maneuver, fromPosition, shipBase);
+
+            if (toPosition && Position.isInPlayArea(toPosition.getX(), toPosition.getY()))
+            {
+                validManeuvers.push(maneuver);
+                var weapon = token.getPrimaryWeapon();
+
+                for (var i = 0; i < defenders.length; i++)
+                {
+                    var defender = defenders[i];
+                    var defenderPosition = environment.getPositionFor(defender);
+
+                    if (weapon.isDefenderTargetable(token, toPosition, defender, defenderPosition))
+                    {
+                        var range = rangeRuler.getRange(token, toPosition, defender, defenderPosition);
+
+                        if (range === Range.ONE)
+                        {
+                            validManeuversR1.push(maneuver);
+                        }
+                        else if (range === Range.TWO)
+                        {
+                            validManeuversR2.push(maneuver);
+                        }
+                        else if (range === Range.THREE)
+                        {
+                            validManeuversR3.push(maneuver);
+                        }
+                    }
+                }
+            }
+        });
+
+        LOGGER.trace("validManeuversR1.length = " + validManeuversR1.length + " for " + token);
+
+        var maneuver = validManeuversR1.vizziniRandomElement();
+
+        if (!maneuver)
+        {
+            LOGGER.trace("validManeuversR2.length = " + validManeuversR2.length + " for " + token);
+            maneuver = validManeuversR2.vizziniRandomElement();
+        }
+
+        if (!maneuver)
+        {
+            LOGGER.trace("validManeuversR3.length = " + validManeuversR3.length + " for " + token);
+            maneuver = validManeuversR3.vizziniRandomElement();
+        }
+
+        if (!maneuver)
+        {
+            LOGGER.trace("validManeuvers.length = " + validManeuvers.length + " for " + token);
+            maneuver = validManeuvers.vizziniRandomElement();
+        }
+
+        LOGGER.trace("0 maneuver = " + maneuver + " for " + token);
+
+        if (!maneuver)
+        {
+            // Ship fled the battlefield.
+            maneuver = maneuvers.vizziniRandomElement();
+            LOGGER.trace("1 maneuver = " + maneuver + " for " + token);
+        }
+
+        tokenToManeuver[token] = maneuver;
+    });
+
+    var answer = new PlanningAction(environment, this, tokenToManeuver);
+
+    callback(answer);
+}
+
+MediumAgent.prototype.toString = function()
+{
+    return this.getName() + ", MediumAgent, " + this.getTeam() + ", " + this.getSquadBuilder().getName();
+}
