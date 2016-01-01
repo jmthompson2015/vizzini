9a22e88b94166fec14de2507cd244d70a6c0bc95
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-01-01 16:10:39 -0700

    Initial version.

---
+++ starfightersquadronsweb/src/main/js/UpgradeHeader.js
@@ -0,0 +1,46 @@
+define(function()
+{
+    var UpgradeHeader =
+    {
+        ACTION: "action",
+        ATTACK: "attack",
+        ATTACK_FOCUS: "attackFocus",
+        ATTACK_TARGET_LOCK: "attackTargetLock",
+
+        properties:
+        {
+            "action":
+            {
+                name: "Action: ",
+                value: "action",
+            },
+            "attack":
+            {
+                name: "Attack: ",
+                value: "attack",
+            },
+            "attackFocus":
+            {
+                name: "Attack (Focus): ",
+                value: "attackFocus",
+            },
+            "attackTargetLock":
+            {
+                name: "Attack (Target Lock): ",
+                value: "attackTargetLock",
+            },
+        },
+
+        values: function()
+        {
+            return Object.getOwnPropertyNames(UpgradeHeader.properties);
+        },
+    }
+
+    if (Object.freeze)
+    {
+        Object.freeze(UpgradeHeader);
+    };
+
+    return UpgradeHeader;
+});
---
+++ starfightersquadronsweb/src/test/js/UpgradeHeaderTest.js
@@ -0,0 +1,32 @@
+define([ "UpgradeHeader" ], function(UpgradeHeader)
+{
+    QUnit.module("UpgradeHeader");
+    
+    QUnit.test("UpgradeHeader", function(assert)
+    {
+        var properties = Object.getOwnPropertyNames(UpgradeHeader);
+        var values = UpgradeHeader.values();
+        assert.equal(properties.length - 1 // properties
+        - 1, // values
+        values.length);
+    });
+
+    QUnit.test("UpgradeHeader properties Attack (Focus)", function(assert)
+    {
+        var type = UpgradeHeader.ATTACK_FOCUS;
+        var properties = UpgradeHeader.properties[type];
+        assert.equal(properties.name, "Attack (Focus): ");
+        assert.equal(properties.value, "attackFocus");
+    });
+
+    QUnit.test("UpgradeHeader.values()", function(assert)
+    {
+        var result = UpgradeHeader.values();
+        assert.ok(result);
+        assert.equal(result.length, 4);
+        assert.equal(result[0], UpgradeHeader.ACTION);
+        assert.equal(result[1], UpgradeHeader.ATTACK);
+        assert.equal(result[2], UpgradeHeader.ATTACK_FOCUS);
+        assert.equal(result[3], UpgradeHeader.ATTACK_TARGET_LOCK);
+    });
+});
