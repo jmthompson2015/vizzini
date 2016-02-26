99daa315ee713fe3cad1407dd7e7d0b602d29eea
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-02-26 16:20:20 -0700

    Updated data.

--- aiweb/src/test/js/geneticprogramming/EvaluatorTest.js
+++ aiweb/src/test/js/geneticprogramming/EvaluatorTest.js
@@ -1,4 +1,5 @@
-define([ "Arithmetic", "Evaluator", "Terminal" ], function(Arithmetic, Evaluator, Terminal)
+define([ "Arithmetic", "Evaluator", "GenomeComparator", "Terminal" ], function(Arithmetic, Evaluator, GenomeComparator,
+        Terminal)
 {
     "use strict";
     QUnit.module("Evaluator");
@@ -9,24 +10,37 @@ define([ "Arithmetic", "Evaluator", "Terminal" ], function(Arithmetic, Evaluator
         var population = [];
         population.push(createTree0());
         population.push(createTree1());
+        population.push(createTree2());
         var fitnessCases = createFitnessCases();
         var isMatches = false;
-        var errorThreshold = 0.001;
+        var errorThreshold = 0.1;
         var evaluator = new Evaluator(fitnessCases.contexts, fitnessCases.outputs, isMatches, errorThreshold);
 
         // Run.
         evaluator.evaluate(population);
 
         // Verify.
-        for (var i = 0; i < population.length; i++)
+        var i;
+        for (i = 0; i < population.length; i++)
         {
             assert.ok(population[i].fitness);
-            assert.ok(population[i].fitness > 0.0);
         }
+
+        i = 0;
+        assert.equal(Math.vizziniRound(population[i++].fitness, 4), 3.3595);
+        assert.equal(Math.vizziniRound(population[i++].fitness, 4), 1.0842);
+        assert.equal(Math.vizziniRound(population[i++].fitness, 4), 1000);
+
+        population.sort(GenomeComparator);
+        i = 0;
+        assert.equal(Math.vizziniRound(population[i++].fitness, 4), 1000);
+        assert.equal(Math.vizziniRound(population[i++].fitness, 4), 3.3595);
+        assert.equal(Math.vizziniRound(population[i++].fitness, 4), 1.0842);
     });
 
     function createTree0()
     {
+        // (- (+ x 1) 2) => (x + 1) - 2
         var node3 = new Terminal.Variable("x");
         var node4 = new Terminal.Constant(1);
         var node5 = new Terminal.Constant(2);
@@ -39,10 +53,37 @@ define([ "Arithmetic", "Evaluator", "Terminal" ], function(Arithmetic, Evaluator
     function createTree1()
     {
         var node3 = new Terminal.Constant(2);
-        var node4 = new Terminal.Variable("y");
+        var node4 = new Terminal.Variable("x");
         var node5 = new Terminal.Constant(3);
         var node2 = new Arithmetic.Subtract([ node3, node4 ]);
         var node1 = new Arithmetic.Add([ node2, node5 ]);
+
+        return node1;
+    }
+
+    function createTree2()
+    {
+        var node17 = new Terminal.Variable("x");
+        var node18 = new Terminal.Variable("x");
+        var node19 = new Terminal.Variable("x");
+        var node16 = new Arithmetic.Multiply([ node17, node18 ]);
+        var node15 = new Arithmetic.Add([ node16, node19 ]);
+
+        var node12 = new Terminal.Variable("x");
+        var node13 = new Terminal.Variable("x");
+        var node14 = new Terminal.Variable("x");
+        var node11 = new Arithmetic.Multiply([ node12, node13 ]);
+        var node10 = new Arithmetic.Multiply([ node11, node14 ]);
+        var node9 = new Arithmetic.Add([ node10, node15 ]);
+
+        var node5 = new Terminal.Variable("x");
+        var node6 = new Terminal.Variable("x");
+        var node7 = new Terminal.Variable("x");
+        var node8 = new Terminal.Variable("x");
+        var node4 = new Arithmetic.Multiply([ node5, node6 ]);
+        var node3 = new Arithmetic.Multiply([ node4, node7 ]);
+        var node2 = new Arithmetic.Multiply([ node3, node8 ]);
+        var node1 = new Arithmetic.Add([ node2, node9 ]);
 
         return node1;
     }
--- aiweb/src/test/js/geneticprogramming/GenomeComparatorTest.js
+++ aiweb/src/test/js/geneticprogramming/GenomeComparatorTest.js
@@ -1,9 +1,10 @@
-define([ "Arithmetic", "GenomeComparator", "Terminal" ], function(Arithmetic, GenomeComparator, Terminal)
+define([ "Arithmetic", "CountVisitor", "GenomeComparator", "Terminal" ], function(Arithmetic, CountVisitor,
+        GenomeComparator, Terminal)
 {
     "use strict";
     QUnit.module("GenomeComparator");
 
-    QUnit.test("create()", function(assert)
+    QUnit.test("compare()", function(assert)
     {
         // Setup.
         var genome0 = createTree0();
@@ -13,11 +14,43 @@ define([ "Arithmetic", "GenomeComparator", "Terminal" ], function(Arithmetic, Ge
         var genome2 = createTree2();
         genome2.fitness = 12.3;
         var comparator = GenomeComparator;
+        var population = [];
+        population.push(genome0);
+        population.push(genome1);
+        population.push(genome2);
 
         // Run / Verify.
         assert.equal(comparator(genome0, genome0), 0);
         assert.ok(comparator(genome0, genome1) > 0);
+        assert.equal(comparator(genome0, genome1), 33.3);
         assert.equal(comparator(genome0, genome2), 2);
+    });
+
+    QUnit.test("sort()", function(assert)
+    {
+        // Setup.
+        var genome0 = createTree0();
+        genome0.fitness = 12.3;
+        var genome1 = createTree1();
+        genome1.fitness = 45.6;
+        var genome2 = createTree2();
+        genome2.fitness = 12.3;
+        var population = [];
+        population.push(genome0);
+        population.push(genome1);
+        population.push(genome2);
+
+        // Run.
+        population.sort(GenomeComparator);
+
+        // Verify.
+        var i = 0;
+        assert.equal(population[i].fitness, 45.6);
+        assert.equal((new CountVisitor(population[i++])).nodeCount(), 5);
+        assert.equal(population[i].fitness, 12.3);
+        assert.equal((new CountVisitor(population[i++])).nodeCount(), 5);
+        assert.equal(population[i].fitness, 12.3);
+        assert.equal((new CountVisitor(population[i++])).nodeCount(), 7);
     });
 
     function createTree0()
