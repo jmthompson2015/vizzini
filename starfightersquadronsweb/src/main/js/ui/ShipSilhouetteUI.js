8a404fd493c93295d46697ce5dcc23a59b189a41
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-03-03 10:26:56 -0700

    Initial version.

---
+++ starfightersquadronsweb/src/main/js/ui/ShipSilhouetteUI.js
@@ -0,0 +1,28 @@
+define([ "Ship", "ShipTeam" ], function(Ship, ShipTeam)
+{
+    "use strict";
+    var ShipSilhouetteUI = React.createClass(
+    {
+        render: function()
+        {
+            InputValidator.validateNotNull("shipKey", this.props.shipKey);
+
+            var shipKey = this.props.shipKey;
+            var shipName0 = Ship.properties[shipKey].name;
+            var shipName = shipName0.replace("/", "_");
+            shipName = shipName.replace(" ", "_");
+            var fileString = imageBase + "silhouette/" + shipName + ".png";
+            var myKey = (this.props.key !== undefined ? this.props.key : 0);
+
+            return React.DOM.img(
+            {
+                key: myKey,
+                className: "shipSilhouetteUIImage",
+                src: fileString,
+                title: shipName0,
+            });
+        },
+    });
+
+    return ShipSilhouetteUI;
+});
