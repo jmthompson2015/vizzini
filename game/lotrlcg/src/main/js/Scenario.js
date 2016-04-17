391518871fc7ca23ee7e95d4e4a835aa2e4780e7
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-04-17 09:35:37 -0600

    Initial version.

---
+++ game/lotrlcg/src/main/js/Scenario.js
@@ -0,0 +1,38 @@
+define([ "EncounterSet" ], function(EncounterSet)
+{
+    var Scenario =
+    {
+        PASSAGE_THROUGH_MIRKWOOD: "passageThroughMirkwood",
+        THE_HUNT_FOR_GOLLUM: "theHuntForGollum",
+
+        properties:
+        {
+            "passageThroughMirkwood":
+            {
+                name: "Passage Through Mirkwood",
+                encounterSetKeys: [ EncounterSet.PASSAGE_THROUGH_MIRKWOOD, EncounterSet.SPIDERS_OF_MIRKWOOD,
+                        EncounterSet.DOL_GULDUR_ORCS ],
+                value: "passageThroughMirkwood",
+            },
+            "theHuntForGollum":
+            {
+                name: "The Hunt for Gollum",
+                encounterSetKeys: [ EncounterSet.THE_HUNT_FOR_GOLLUM, EncounterSet.JOURNEY_DOWN_THE_ANDUIN,
+                        EncounterSet.SAURONS_REACH ],
+                value: "theHuntForGollum",
+            },
+        },
+
+        values: function()
+        {
+            return Object.getOwnPropertyNames(Scenario.properties);
+        },
+    };
+
+    if (Object.freeze)
+    {
+        Object.freeze(Scenario);
+    }
+
+    return Scenario;
+});
