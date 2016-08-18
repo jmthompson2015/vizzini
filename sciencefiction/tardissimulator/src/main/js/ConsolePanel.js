87abb745cef6e342b9479d891b396c1eb6e6d7ef
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-08-18 11:02:11 -0600

    Moved the panel images to the html directory to avoid cross domain issues with Safari.

--- sciencefiction/tardissimulator/src/main/js/ConsolePanel.js
+++ sciencefiction/tardissimulator/src/main/js/ConsolePanel.js
@@ -15,33 +15,33 @@ define(function()
             "panel1":
             {
                 name: "Fabrication & Information Systems",
-                image: "../resources/images/panel1.jpg",
+                image: "panel1.jpg",
                 value: "panel1",
             },
             "panel2":
             {
                 name: "Navigation",
-                image: "../resources/images/panel2.jpg",
+                image: "panel2.jpg",
                 value: "panel2",
             },
             "panel3":
             {
                 name: "Mechanical & Master Control",
-                image: "../resources/images/panel3.jpg",
+                image: "panel3.jpg",
                 value: "panel3",
             // scanner switch
             },
             "panel4":
             {
                 name: "Communications & Exterior Monitor",
-                image: "../resources/images/panel4.jpg",
+                image: "panel4.jpg",
                 value: "panel4",
             // radiation read-outs
             },
             "panel5":
             {
                 name: "Helm & Dematerialisation Systems",
-                image: "../resources/images/panel5.jpg",
+                image: "panel5.jpg",
                 value: "panel5",
             // auxiliary power control
             // main power bus levers
@@ -49,7 +49,7 @@ define(function()
             "panel6":
             {
                 name: "Diagnostic & Internal Ship Systems",
-                image: "../resources/images/panel6.jpg",
+                image: "panel6.jpg",
                 value: "panel6",
             // door switch
             // scanner control
