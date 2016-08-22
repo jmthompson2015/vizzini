32bc3e272dab10cff2f94daf596178bfcb432601
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-08-22 13:10:19 -0600

    Refined the pieces of the TimeRotorUI.

--- sciencefiction/tardissimulator/src/main/js/process/ui/TimeRotorUI.js
+++ sciencefiction/tardissimulator/src/main/js/process/ui/TimeRotorUI.js
@@ -37,15 +37,6 @@ define(function()
         var baseHeight = 0.46 * coverRatio2;
         var tubeHeight = 3.4 * coverRatio2;
 
-        var tubeColor = 0xFF2211;
-        var tubeMaterial = new THREE.MeshStandardMaterial(
-        {
-            color: tubeColor,
-            metalness: 0.4,
-            opacity: 0.75,
-            shading: THREE.FlatShading,
-        });
-
         this.createBase = function()
         {
             var geometry = new THREE.CylinderGeometry(baseRadius, baseRadius, baseHeight, 32);
@@ -71,8 +62,8 @@ define(function()
             var material = new THREE.MeshPhongMaterial(
             {
                 color: 0xFFFFFF,
-                transparent: true,
                 opacity: 0.35,
+                transparent: true,
             });
 
             geometry.computeFaceNormals();
@@ -85,38 +76,65 @@ define(function()
             return answer;
         };
 
-        this.createSidePiece = function()
+        this.createDome = function()
         {
-            var geometry = new THREE.BoxGeometry(1.4 * tubePositionRadius, tubeHeight, 2.0);
+            var geometry = new THREE.SphereGeometry(domeRadius, 8, 6, 0, Math.PI);
 
             geometry.computeFaceNormals();
             geometry.computeVertexNormals();
 
-            return new THREE.Mesh(geometry, tubeMaterial);
+            var material = new THREE.MeshStandardMaterial(
+            {
+                color: 0xFFFFFF,
+                metalness: 0.5,
+            });
+
+            var answer = new THREE.Mesh(geometry, material);
+            var y = tubeHeight / 2.0;
+            answer.position.set(0, y, 0);
+            answer.rotation.x = d2r(-90.0);
+
+            return answer;
+        };
+
+        this.createSidePiece = function()
+        {
+            var geometry = new THREE.BoxGeometry(1.2 * tubePositionRadius, tubeHeight, 0.25);
+
+            geometry.computeFaceNormals();
+            geometry.computeVertexNormals();
+
+            var material = new THREE.MeshStandardMaterial(
+            {
+                color: 0xFF8888,
+                metalness: 0,
+                opacity: 0.75,
+            });
+
+            return new THREE.Mesh(geometry, material);
         };
 
         this.createTube = function()
         {
-            var tubeGeometry = new THREE.CylinderGeometry(tubeRadius, tubeRadius, tubeHeight, 16);
+            var geometry = new THREE.CylinderGeometry(tubeRadius, tubeRadius, tubeHeight, 16);
 
-            tubeGeometry.computeFaceNormals();
-            tubeGeometry.computeVertexNormals();
+            geometry.computeFaceNormals();
+            geometry.computeVertexNormals();
 
-            var domeGeometry = new THREE.SphereGeometry(domeRadius, 8, 6, 0, Math.PI);
-            var domeMaterial = new THREE.MeshStandardMaterial(
+            var material = new THREE.MeshStandardMaterial(
             {
-                color: 0xFFFFFF,
-                metalness: 0.4,
+                color: 0xFF2211,
+                metalness: 0,
+                opacity: 0.75,
             });
 
-            domeGeometry.computeFaceNormals();
-            domeGeometry.computeVertexNormals();
+            return new THREE.Mesh(geometry, material);
+        };
 
-            var tube = new THREE.Mesh(tubeGeometry, tubeMaterial);
-            var dome = new THREE.Mesh(domeGeometry, domeMaterial);
-            var y = tubeHeight / 2.0;
-            dome.position.set(0, y, 0);
-            dome.rotation.x = d2r(-90.0);
+        this.createTubeAssembly = function()
+        {
+            var tube = this.createTube();
+            var dome = this.createDome();
 
             tube.add(dome);
 
@@ -135,7 +153,7 @@ define(function()
 
         for (i = 0; i < 3; i++)
         {
-            var tube = this.createTube();
+            var tube = this.createTubeAssembly();
             x = tubePositionRadius * Math.sin(d2r(i * 120.0));
             y = baseHeight + (tubeHeight / 2.0);
             z = tubePositionRadius * Math.cos(d2r(i * 120.0));
@@ -143,7 +161,7 @@ define(function()
             root.add(tube);
         }
 
-        var factor = 0.7;
+        var factor = 0.85;
 
         for (i = 0; i < 3; i++)
         {
