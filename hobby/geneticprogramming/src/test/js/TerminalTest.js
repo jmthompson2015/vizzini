1085b0ed8c9482ef25912870a5e43705c3d19ff5
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-09-18 13:27:08 -0600

    Initial version.

---
+++ hobby/geneticprogramming/src/test/js/TerminalTest.js
@@ -0,0 +1,35 @@
+define([ "Terminal" ], function(Terminal)
+{
+    "use strict";
+    QUnit.module("Terminal");
+
+    QUnit.test("Constant()", function(assert)
+    {
+        // Setup.
+
+        // Run.
+        var result = new Terminal.Constant(12);
+
+        // Verify.
+        assert.ok(result);
+        assert.equal(result.evaluate(), 12);
+        assert.equal(result.toString(), "Constant 12");
+    });
+
+    QUnit.test("Variable()", function(assert)
+    {
+        // Setup.
+        var context =
+        {
+            "x": 12,
+        };
+
+        // Run.
+        var result = new Terminal.Variable("x");
+
+        // Verify.
+        assert.ok(result);
+        assert.equal(result.evaluate(context), 12);
+        assert.equal(result.toString(), "Variable x");
+    });
+});
