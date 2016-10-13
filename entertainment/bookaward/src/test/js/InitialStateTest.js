bf402fec6bda9d563fc9280adf2b281b5ba62232
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-10-13 13:57:30 -0600

    Added Crime & Beyond books.

--- entertainment/bookaward/src/test/js/InitialStateTest.js
+++ entertainment/bookaward/src/test/js/InitialStateTest.js
@@ -10,7 +10,7 @@ define(["InitialState"], function(InitialState)
 
         // Verify.
         assert.ok(result.books);
-        assert.equal(result.books.length, 68);
+        assert.equal(result.books.length, 85);
         assert.ok(result.bookToNomination);
         assert.equal(result.bookToNomination[result.books[0]].length, 1);
         assert.equal(result.bookToNomination[result.books[result.books.length - 1]].length, 1);
