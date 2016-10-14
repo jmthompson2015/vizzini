74a3f7b6121ab5e701d5d5fcb79eeb365042e84f
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-10-13 21:08:06 -0600

    Added the Dagger award.

--- entertainment/bookaward/src/test/js/process/SYKMNomineeFetcherTest.js
+++ entertainment/bookaward/src/test/js/process/SYKMNomineeFetcherTest.js
@@ -132,49 +132,6 @@ define(["Award", "process/SYKMNomineeFetcher"], function(Award, SYKMNomineeFetch
         assert.ok(true);
     });
 
-    QUnit.skip("receiveData() Dagger", function(assert)
-    {
-        // Setup.
-        var award = Award.properties[Award.DAGGER];
-        var callback = function(books, bookToNomination)
-        {
-            assert.ok(books);
-            assert.equal(books.length, 15);
-
-            var i = 0;
-            assert.equal(books[i].title(), "Long Upon the Land");
-            assert.equal(books[i].author(), "Margaret Maron");
-            var nominations = bookToNomination[books[i]];
-            assert.ok(nominations);
-            assert.equal(nominations.length, 1);
-            var j = 0;
-            assert.equal(nominations[j].award(), award);
-            var category = award.categories.properties[award.categories.CONTEMPORARY];
-            assert.equal(nominations[j].category(), category);
-            assert.equal(nominations[j].year(), 2015);
-
-            i = books.length - 1;
-            assert.equal(books[i].title(), "Just Killing Time");
-            assert.equal(books[i].author(), "Julianne Holmes");
-            nominations = bookToNomination[books[i]];
-            assert.ok(nominations);
-            assert.equal(nominations.length, 1);
-            j = 0;
-            assert.equal(nominations[j].award(), award);
-            category = award.categories.properties[award.categories.FIRST];
-            assert.equal(nominations[j].category(), category);
-            assert.equal(nominations[j].year(), 2015);
-        };
-        var fetcher = new SYKMNomineeFetcher(award, callback);
-        var xmlDocument = load(award.name);
-
-        // Run.
-        fetcher.receiveData(xmlDocument);
-
-        // Verify.
-        assert.ok(true);
-    });
-
     QUnit.test("receiveData() Edgar", function(assert)
     {
         // Setup.
