8c06c9531a5da350447ecd5f151f32f79595f90a
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-10-14 16:02:53 -0600

    Merged SYKMDaggerNomineeFetcher into SYKMNomineeFetcher.

--- entertainment/bookaward/src/test/js/process/SYKMNomineeFetcherTest.js
+++ entertainment/bookaward/src/test/js/process/SYKMNomineeFetcherTest.js
@@ -134,6 +134,56 @@ define(["Award", "process/SYKMNomineeFetcher"], function(Award, SYKMNomineeFetch
         assert.ok(true);
     });
 
+    QUnit.test("receiveData() Dagger", function(assert)
+    {
+        // Setup.
+        var award = Award.properties[Award.DAGGER];
+        var callback = function(books, bookToNomination)
+        {
+            assert.ok(books);
+            assert.equal(books.length, 23);
+
+            var i = 0;
+            assert.equal(books[i].title(), "Dodgers");
+            assert.equal(books[i].author(), "Bill Beverly");
+            var nominations = bookToNomination[books[i]];
+            assert.ok(nominations);
+            assert.equal(nominations.length, 2);
+            var j = 0;
+            assert.equal(nominations[j].award(), award);
+            var category = award.categories.properties[award.categories.GOLD];
+            assert.equal(nominations[j].category(), category);
+            assert.equal(nominations[j].year(), 2016);
+            assert.ok(nominations[j].isWinner());
+            j = 1;
+            assert.equal(nominations[j].award(), award);
+            category = award.categories.properties[award.categories.FIRST];
+            assert.equal(nominations[j].category(), category);
+            assert.equal(nominations[j].year(), 2016);
+
+            i = books.length - 1;
+            assert.equal(books[i].title(), "Striking Murder");
+            assert.equal(books[i].author(), "A.J. Wright");
+            nominations = bookToNomination[books[i]];
+            assert.ok(nominations);
+            assert.equal(nominations.length, 1);
+            j = 0;
+            assert.equal(nominations[j].award(), award);
+            category = award.categories.properties[award.categories.HISTORICAL];
+            assert.equal(nominations[j].category(), category);
+            assert.equal(nominations[j].year(), 2016);
+            assert.ok(!nominations[j].isWinner());
+        };
+        var fetcher = new SYKMNomineeFetcher(award, callback);
+        var xmlDocument = load(award.name);
+
+        // Run.
+        fetcher.receiveData(xmlDocument);
+
+        // Verify.
+        assert.ok(true);
+    });
+
     QUnit.test("receiveData() Edgar", function(assert)
     {
         // Setup.
