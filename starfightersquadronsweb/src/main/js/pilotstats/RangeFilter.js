8589d5e96705fccd3e302e0d0140f6d246d5b712
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-10-21 10:57:52 -0600

    Changed to save the filters to local storage.

--- starfightersquadronsweb/src/main/js/pilotstats/RangeFilter.js
+++ starfightersquadronsweb/src/main/js/pilotstats/RangeFilter.js
@@ -44,11 +44,37 @@ define(function()
             return (!isMinEnabled || minValue <= value) && (!isMaxEnabled || value <= maxValue);
         };
 
+        this.toObject = function()
+        {
+            return (
+            {
+                type: "RangeFilter",
+                columnKey: columnKey,
+                isMinEnabled: isMinEnabled,
+                minValue: minValue,
+                isMaxEnabled: isMaxEnabled,
+                maxValue: maxValue,
+            });
+        };
+
         this.toString = function()
         {
             return "RangeFilter (" + isMinEnabled + " " + minValue + "\u2264" + columnKey + "\u2264" + isMaxEnabled + " " + maxValue + ")";
         };
     }
+
+    RangeFilter.fromObject = function(object)
+    {
+        InputValidator.validateNotNull("object", object);
+
+        var columnKey = object.columnKey;
+        var isMinEnabled = object.isMinEnabled;
+        var minValue = object.minValue;
+        var isMaxEnabled = object.isMaxEnabled;
+        var maxValue = object.maxValue;
+
+        return new RangeFilter(columnKey, isMinEnabled, minValue, isMaxEnabled, maxValue);
+    };
 
     return RangeFilter;
 });
