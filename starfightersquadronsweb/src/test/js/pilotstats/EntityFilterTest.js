6d3e98c6cb186d194cf056a62e1c42fc6fca84e6
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-10-21 10:59:35 -0600

    Changed to save the filters to local storage.

--- starfightersquadronsweb/src/test/js/pilotstats/EntityFilterTest.js
+++ starfightersquadronsweb/src/test/js/pilotstats/EntityFilterTest.js
@@ -17,20 +17,62 @@ define(["Team", "pilotstats/EntityFilter"], function(Team, EntityFilter)
         assert.equal(result.values(), values);
     });
 
-    QUnit.test("EntityFilter()", function(assert)
+    QUnit.test("EntityFilter.fromObject()", function(assert)
     {
         // Setup.
-        var columnKey = "faction";
+        var object = {
+            type: "EntityFilter",
+            columnKey: "factionKey",
+            values: [Team.FIRST_ORDER, Team.IMPERIAL],
+        };
+
+        // Run.
+        var result = EntityFilter.fromObject(object);
+
+        // Verify.
+        assert.ok(result);
+        assert.equal(result.columnKey(), "factionKey");
+        assert.ok(result.values());
+        var values = result.values();
+        assert.equal(values.length, 2);
+        assert.equal(values[0], Team.FIRST_ORDER);
+        assert.equal(values[1], Team.IMPERIAL);
+    });
+
+    QUnit.test("passes()", function(assert)
+    {
+        // Setup.
+        var columnKey = "factionKey";
         var values0 = [Team.REBEL, Team.RESISTANCE];
         var values1 = [Team.FIRST_ORDER, Team.IMPERIAL];
         var data = (
         {
-            faction: Team.IMPERIAL,
+            factionKey: Team.IMPERIAL,
         });
 
         // Run / Verify.
         assert.ok(!(new EntityFilter(columnKey, values0)).passes(data));
         assert.ok(!(new EntityFilter("bogus", values0)).passes(data));
         assert.ok((new EntityFilter(columnKey, values1)).passes(data));
+    });
+
+    QUnit.test("toObject()", function(assert)
+    {
+        // Setup.
+        var columnKey = "factionKey";
+        var values = [Team.FIRST_ORDER, Team.IMPERIAL];
+        var filter = new EntityFilter(columnKey, values);
+
+        // Run.
+        var result = filter.toObject();
+
+        // Verify.
+        assert.ok(result);
+        assert.equal(result.type, "EntityFilter");
+        assert.equal(result.columnKey, "factionKey");
+        assert.ok(result.values);
+        assert.equal(result.values.length, 2);
+        assert.equal(result.values[0], Team.FIRST_ORDER);
+        assert.equal(result.values[1], Team.IMPERIAL);
     });
 });
