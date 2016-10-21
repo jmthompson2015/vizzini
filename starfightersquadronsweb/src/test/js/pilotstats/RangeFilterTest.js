b27c6cdeb6a02149e28ff9a3f32d47bcd814c77a
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-10-21 11:00:03 -0600

    Changed to save the filters to local storage.

--- starfightersquadronsweb/src/test/js/pilotstats/RangeFilterTest.js
+++ starfightersquadronsweb/src/test/js/pilotstats/RangeFilterTest.js
@@ -23,7 +23,31 @@ define(["pilotstats/RangeFilter"], function(RangeFilter)
         assert.equal(result.maxValue(), maxValue);
     });
 
-    QUnit.test("RangeFilter()", function(assert)
+    QUnit.test("RangeFilter.fromObject()", function(assert)
+    {
+        // Setup.
+        var object = {
+            type: "RangeFilter",
+            columnKey: "pilotSkill",
+            isMinEnabled: false,
+            minValue: 1,
+            isMaxEnabled: true,
+            maxValue: 10,
+        };
+
+        // Run.
+        var result = RangeFilter.fromObject(object);
+
+        // Verify.
+        assert.ok(result);
+        assert.equal(result.columnKey(), "pilotSkill");
+        assert.equal(result.isMinEnabled(), false);
+        assert.equal(result.minValue(), 1);
+        assert.equal(result.isMaxEnabled(), true);
+        assert.equal(result.maxValue(), 10);
+    });
+
+    QUnit.test("passes()", function(assert)
     {
         // Setup.
         var columnKey = "pilotSkill";
@@ -45,5 +69,28 @@ define(["pilotstats/RangeFilter"], function(RangeFilter)
 
         assert.ok(!(new RangeFilter(columnKey, true, 6, isMaxEnabled, maxValue)).passes(data));
         assert.ok(!(new RangeFilter(columnKey, isMinEnabled, minValue, true, 4)).passes(data));
+    });
+
+    QUnit.test("toObject()", function(assert)
+    {
+        // Setup.
+        var columnKey = "factionKey";
+        var isMinEnabled = false;
+        var minValue = 1;
+        var isMaxEnabled = true;
+        var maxValue = 10;
+        var filter = new RangeFilter(columnKey, isMinEnabled, minValue, isMaxEnabled, maxValue);
+
+        // Run.
+        var result = filter.toObject();
+
+        // Verify.
+        assert.ok(result);
+        assert.equal(result.type, "RangeFilter");
+        assert.equal(result.columnKey, columnKey);
+        assert.equal(result.isMinEnabled, isMinEnabled);
+        assert.equal(result.minValue, minValue);
+        assert.equal(result.isMaxEnabled, isMaxEnabled);
+        assert.equal(result.maxValue, maxValue);
     });
 });
