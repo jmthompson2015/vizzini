1c3ac2b56b8df73a57435e5f1a038453bc52dfeb
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-11-02 11:36:38 -0600

    Initial version.

---
+++ starfightersquadronsweb/src/test/js/CombatStateTest.js
@@ -0,0 +1,92 @@
+define(["AttackDice", "CombatState", "DefenseDice", "RangeRuler", "process/CombatAction"],
+    function(AttackDice, CombatState, DefenseDice, RangeRuler, CombatAction)
+    {
+        "use strict";
+        QUnit.module("CombatState");
+
+        QUnit.test("attackDice()", function(assert)
+        {
+            // Setup.
+            var initialSize = 3;
+            var attackDice = new AttackDice(initialSize);
+            var combatState = new CombatState();
+            assert.ok(!combatState.attackDice());
+
+            // Run.
+            var result = combatState.attackDice(attackDice);
+
+            // Verify.
+            assert.ok(result);
+            assert.equal(result, attackDice);
+        });
+
+        QUnit.test("combatAction()", function(assert)
+        {
+            // Setup.
+            var environment = {};
+            var adjudicator = {};
+            var attacker = {};
+            var attackerPosition = {};
+            var weapon = {};
+            var defender = {};
+            var defenderPosition = {};
+            var callback = {};
+            var combatAction = new CombatAction(environment, adjudicator, attacker, attackerPosition, weapon, defender,
+                defenderPosition, callback);
+            var combatState = new CombatState();
+            assert.ok(!combatState.combatAction());
+
+            // Run.
+            var result = combatState.combatAction(combatAction);
+
+            // Verify.
+            assert.ok(result);
+            assert.equal(result, combatAction);
+        });
+
+        QUnit.test("defenseDice()", function(assert)
+        {
+            // Setup.
+            var initialSize = 3;
+            var defenseDice = new DefenseDice(initialSize);
+            var combatState = new CombatState();
+            assert.ok(!combatState.defenseDice());
+
+            // Run.
+            var result = combatState.defenseDice(defenseDice);
+
+            // Verify.
+            assert.ok(result);
+            assert.equal(result, defenseDice);
+        });
+
+        QUnit.test("isDefenderHit()", function(assert)
+        {
+            // Setup.
+            var isDefenderHit = true;
+            var combatState = new CombatState();
+            assert.ok(!combatState.isDefenderHit());
+
+            // Run.
+            var result = combatState.isDefenderHit(isDefenderHit);
+
+            // Verify.
+            assert.ok(result);
+            assert.equal(result, isDefenderHit);
+        });
+
+        QUnit.test("rangeKey()", function(assert)
+        {
+            // Setup.
+            var rangeKey = RangeRuler.ONE;
+            var combatState = new CombatState();
+            assert.ok(!combatState.rangeKey());
+
+            // Run.
+            var result = combatState.rangeKey(rangeKey);
+
+            // Verify.
+            assert.ok(result);
+            assert.equal(result, rangeKey);
+        });
+    });
