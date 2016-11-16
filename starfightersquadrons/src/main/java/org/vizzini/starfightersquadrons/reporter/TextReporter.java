ec681300350f576522baaccc6dbc24feb387b610
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-11-15 20:47:02 -0700

    Removed.

--- starfightersquadrons/src/main/java/org/vizzini/starfightersquadrons/reporter/TextReporter.java
+++
@@ -1,9 +0,0 @@
-package org.vizzini.starfightersquadrons.reporter;
-
-/**
- * Defines methods required by a text reporter.
- */
-public interface TextReporter extends Reporter
-{
-    // Nothing to do.
-}
