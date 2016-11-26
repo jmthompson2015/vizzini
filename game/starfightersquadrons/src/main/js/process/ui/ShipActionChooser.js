db357a4a0879ed7f67d5831847dfc664797f024e
jmthompson2015 <softwaredev@jeffreythompson.net>
2016-11-26 14:00:08 -0700

    Changed to use the new AbilityUI.

--- game/starfightersquadrons/src/main/js/process/ui/ShipActionChooser.js
+++ game/starfightersquadrons/src/main/js/process/ui/ShipActionChooser.js
@@ -1,121 +1,142 @@
-define(["ShipAction", "process/ui/ShipActionUI"], function(ShipAction, ShipActionUI)
-{
-    "use strict";
-    var ShipActionChooser = React.createClass(
+define(["ShipAction", "process/ui/AbilityUI", "process/ui/ShipActionUI"],
+    function(ShipAction, AbilityUI, ShipActionUI)
     {
-        propTypes:
+        "use strict";
+        var ShipActionChooser = React.createClass(
         {
-            callback: React.PropTypes.func.isRequired,
-            imageBase: React.PropTypes.string.isRequired,
-            shipActions: React.PropTypes.array.isRequired,
-            token: React.PropTypes.object.isRequired,
-        },
-
-        getInitialState: function()
-        {
-            var shipActions = this.props.shipActions;
-
-            return (
+            propTypes:
             {
-                selected: (shipActions.length > 0 ? shipActions[0] : undefined),
-            });
-        },
+                callback: React.PropTypes.func.isRequired,
+                imageBase: React.PropTypes.string.isRequired,
+                shipActions: React.PropTypes.array.isRequired,
+                token: React.PropTypes.object.isRequired,
+            },
 
-        render: function()
-        {
-            var token = this.props.token;
-            var message = "Active Ship: " + token.name();
-            var shipActions = this.props.shipActions;
-            var imageBase = this.props.imageBase;
-            var idFunction = function(value)
+            getInitialState: function()
             {
-                return value.toString();
-            };
-            var labelFunction = function(value)
-            {
-                var answer = React.DOM.span(
-                {}, React.createElement(ShipActionUI,
+                var shipActions = this.props.shipActions;
+
+                return (
                 {
-                    shipAction: ShipAction.properties[value.shipActionKey()],
-                    imageBase: imageBase,
-                }), " ", value.toString());
-                return answer;
-            };
-            var initialValue = (shipActions.length > 0 ? shipActions[0] : undefined);
-            var initialInput = React.createElement(InputPanel,
-            {
-                type: "radio",
-                values: shipActions,
-                name: "shipActionRadio",
-                idFunction: idFunction,
-                labelFunction: labelFunction,
-                initialValues: initialValue,
-                onChange: this.selectionChanged,
-                panelClass: "optionPaneInput",
-            });
-            var cancelButton = React.DOM.button(
-            {
-                key: 0,
-                onClick: this.cancel
-            }, "Cancel");
-            var okButton = React.DOM.button(
-            {
-                key: 1,
-                onClick: this.ok
-            }, "OK");
-            var buttons = React.DOM.span(
-            {}, [cancelButton, okButton]);
+                    selected: (shipActions.length > 0 ? shipActions[0] : undefined),
+                });
+            },
 
-            return React.createElement(OptionPane,
+            render: function()
             {
-                panelClass: "optionPane",
-                title: "Select Action",
-                titleClass: "optionPaneTitle",
-                message: message,
-                messageClass: "optionPaneMessage",
-                initialInput: initialInput,
-                buttons: buttons,
-                buttonsClass: "optionPaneButtons"
-            });
-        },
-
-        selectionChanged: function(event)
-        {
-            var selected = event.target.id;
-            var shipActions = this.props.shipActions;
-
-            for (var i = 0; i < shipActions.length; i++)
-            {
-                var shipAction = shipActions[i];
-
-                if (shipAction.toString() === selected)
+                var token = this.props.token;
+                var message = "Active Ship: " + token.name();
+                var shipActions = this.props.shipActions;
+                var imageBase = this.props.imageBase;
+                var idFunction = function(value)
                 {
-                    selected = shipAction;
-                    break;
+                    return value.toString();
+                };
+                var labelFunction = function(value)
+                {
+                    var answer;
+                    if (value.shipActionKey() === "criticalDamage")
+                    {
+                        answer = React.createElement(AbilityUI.Damage,
+                        {
+                            damage: value.damage(),
+                            imageBase: imageBase,
+                        });
+                    }
+                    else if (value.shipActionKey() === "upgrade")
+                    {
+                        answer = React.createElement(AbilityUI.Upgrade,
+                        {
+                            upgrade: value.upgrade(),
+                            imageBase: imageBase,
+                        });
+                    }
+                    else
+                    {
+                        answer = React.DOM.span(
+                        {}, React.createElement(ShipActionUI,
+                        {
+                            shipAction: ShipAction.properties[value.shipActionKey()],
+                            imageBase: imageBase,
+                        }), " ", value.toString());
+                    }
+                    return answer;
+                };
+                var initialValue = (shipActions.length > 0 ? shipActions[0] : undefined);
+                var initialInput = React.createElement(InputPanel,
+                {
+                    type: "radio",
+                    values: shipActions,
+                    name: "shipActionRadio",
+                    idFunction: idFunction,
+                    labelFunction: labelFunction,
+                    initialValues: initialValue,
+                    onChange: this.selectionChanged,
+                    panelClass: "optionPaneInput",
+                });
+                var cancelButton = React.DOM.button(
+                {
+                    key: 0,
+                    onClick: this.cancel
+                }, "Cancel");
+                var okButton = React.DOM.button(
+                {
+                    key: 1,
+                    onClick: this.ok
+                }, "OK");
+                var buttons = React.DOM.span(
+                {}, [cancelButton, okButton]);
+
+                return React.createElement(OptionPane,
+                {
+                    panelClass: "optionPane",
+                    title: "Select Action",
+                    titleClass: "optionPaneTitle",
+                    message: message,
+                    messageClass: "optionPaneMessage",
+                    initialInput: initialInput,
+                    buttons: buttons,
+                    buttonsClass: "optionPaneButtons"
+                });
+            },
+
+            selectionChanged: function(event)
+            {
+                var selected = event.target.id;
+                var shipActions = this.props.shipActions;
+
+                for (var i = 0; i < shipActions.length; i++)
+                {
+                    var shipAction = shipActions[i];
+
+                    if (shipAction.toString() === selected)
+                    {
+                        selected = shipAction;
+                        break;
+                    }
                 }
-            }
 
-            LOGGER.debug("selectionChanged() selected = " + selected + " typeof selected = " + (typeof selected));
+                LOGGER.debug("selectionChanged() selected = " + selected + " typeof selected = " + (typeof selected));
 
-            this.setState(
+                this.setState(
+                {
+                    selected: selected,
+                });
+            },
+
+            cancel: function()
             {
-                selected: selected,
-            });
-        },
+                LOGGER.debug("Cancel clicked");
+                this.props.callback(undefined);
+            },
 
-        cancel: function()
-        {
-            LOGGER.debug("Cancel clicked");
-            this.props.callback(undefined);
-        },
+            ok: function()
+            {
+                var selected = this.state.selected;
+                LOGGER.debug("OK clicked, selected = " + selected);
+                this.props.callback(selected);
+            },
+        });
 
-        ok: function()
-        {
-            var selected = this.state.selected;
-            LOGGER.debug("OK clicked, selected = " + selected);
-            this.props.callback(selected);
-        },
+        return ShipActionChooser;
     });
-
-    return ShipActionChooser;
-});
