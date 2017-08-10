33129cff4216703a139287ce43513a6eaa76e31b
jmthompson2015 <softwaredev@jeffreythompson.net>
2017-08-09 20:58:41 -0600

    Rearranged methods into flow order. Moved methods into the prototype.

--- game/starfightersquadrons/src/main/js/process/EventObserver.js
+++ game/starfightersquadrons/src/main/js/process/EventObserver.js
@@ -7,121 +7,9 @@ define(["process/Action", "process/DamageAbility0", "process/PilotAbility0", "pr
       {
          InputValidator.validateNotNull("store", store);
 
-         this.onChange = function(eventQueue)
+         this.store = function()
          {
-            LOGGER.trace("EventObserver.onChange() start");
-
-            if (eventQueue.size > 0)
-            {
-               store.dispatch(Action.dequeueEvent());
-               var eventData = store.getState().eventData;
-
-               if (eventData !== undefined)
-               {
-                  var eventKey = eventData.get("eventKey");
-                  var token = eventData.get("eventToken");
-                  var callback = eventData.get("eventCallback");
-
-                  if (eventKey !== undefined && token !== undefined)
-                  {
-                     var queue = [];
-
-                     if (token.usableDamageAbilities)
-                     {
-                        queue.vizziniAddAll(token.usableDamageAbilities(DamageAbility0, eventKey));
-                     }
-
-                     if (token.usablePilotAbilities)
-                     {
-                        queue.vizziniAddAll(token.usablePilotAbilities(PilotAbility0, eventKey));
-                     }
-
-                     if (token.usableUpgradeAbilities)
-                     {
-                        queue.vizziniAddAll(token.usableUpgradeAbilities(UpgradeAbility0, eventKey));
-                     }
-
-                     if (queue.length > 0)
-                     {
-                        this.performAbilities(eventKey, token, queue, callback);
-                     }
-                     else
-                     {
-                        this.finishOnChange(queue, callback);
-                     }
-                  }
-               }
-            }
-
-            LOGGER.trace("EventObserver.onChange() end");
-         };
-
-         this.finishOnChange = function(queue, callback)
-         {
-            LOGGER.trace("EventObserver.finishOnChange() start");
-
-            InputValidator.validateNotNull("queue", queue);
-
-            if (queue.length === 0)
-            {
-               store.dispatch(Action.clearEvent());
-
-               if (callback)
-               {
-                  callback();
-               }
-            }
-
-            LOGGER.trace("EventObserver.finishOnChange() end");
-         };
-
-         this.performAbilities = function(eventKey, token, queue, callback)
-         {
-            LOGGER.trace("EventObserver.performAbilities() start");
-
-            InputValidator.validateNotNull("eventKey", eventKey);
-            InputValidator.validateNotNull("token", token);
-
-            this.processAbilityQueue(queue, store, token, callback);
-
-            LOGGER.trace("EventObserver.performAbilities() end");
-         };
-
-         this.processAbilityQueue = function(queue, store, token, callback)
-         {
-            LOGGER.trace("EventObserver.processAbilityQueue() start");
-
-            if (queue.length === 0)
-            {
-               setTimeout(this.finishOnChange(queue, callback), 10);
-            }
-            else
-            {
-               var ability = queue.shift();
-               var that = this;
-               var myCallback = function()
-               {
-                  that.processAbilityQueue(queue, store, token, callback);
-               };
-
-               if (ability.conditionPasses(store, token))
-               {
-                  var consequent = ability.consequent();
-                  consequent(store, token, myCallback);
-                  var usedAbilities = ability.usedAbilities(token);
-
-                  if (usedAbilities !== undefined)
-                  {
-                     usedAbilities.push(ability.abilityKey);
-                  }
-               }
-               else
-               {
-                  myCallback();
-               }
-            }
-
-            LOGGER.trace("EventObserver.processAbilityQueue() end");
+            return store;
          };
 
          this.select = function(state)
@@ -131,6 +19,115 @@ define(["process/Action", "process/DamageAbility0", "process/PilotAbility0", "pr
 
          var unsubscribe = Observer.observeStore(store, this.select, this.onChange.bind(this), false);
       }
+
+      EventObserver.prototype.onChange = function(eventQueue)
+      {
+         LOGGER.trace("EventObserver.onChange() start");
+
+         if (eventQueue.size > 0)
+         {
+            var store = this.store();
+            store.dispatch(Action.dequeueEvent());
+            var eventData = store.getState().eventData;
+
+            if (eventData !== undefined)
+            {
+               var eventKey = eventData.get("eventKey");
+               var token = eventData.get("eventToken");
+               var callback = eventData.get("eventCallback");
+
+               if (eventKey !== undefined && token !== undefined)
+               {
+                  var queue = [];
+
+                  if (token.usableDamageAbilities)
+                  {
+                     queue.vizziniAddAll(token.usableDamageAbilities(DamageAbility0, eventKey));
+                  }
+
+                  if (token.usablePilotAbilities)
+                  {
+                     queue.vizziniAddAll(token.usablePilotAbilities(PilotAbility0, eventKey));
+                  }
+
+                  if (token.usableUpgradeAbilities)
+                  {
+                     queue.vizziniAddAll(token.usableUpgradeAbilities(UpgradeAbility0, eventKey));
+                  }
+
+                  if (queue.length > 0)
+                  {
+                     this.processAbilityQueue(store, token, queue, callback);
+                  }
+                  else
+                  {
+                     this.finishOnChange(store, callback);
+                  }
+               }
+            }
+         }
+
+         LOGGER.trace("EventObserver.onChange() end");
+      };
+
+      EventObserver.prototype.processAbilityQueue = function(store, token, queue, callback)
+      {
+         LOGGER.trace("EventObserver.processAbilityQueue() start");
+
+         InputValidator.validateNotNull("store", store);
+         InputValidator.validateNotNull("token", token);
+         InputValidator.validateNotNull("queue", queue);
+         // callback optional.
+
+         if (queue.length === 0)
+         {
+            setTimeout(this.finishOnChange(store, callback), 10);
+         }
+         else
+         {
+            var ability = queue.shift();
+            var that = this;
+            var myCallback = function()
+            {
+               that.processAbilityQueue(store, token, queue, callback);
+            };
+
+            if (ability.conditionPasses(store, token))
+            {
+               var consequent = ability.consequent();
+               consequent(store, token, myCallback);
+               var usedAbilities = ability.usedAbilities(token);
+
+               if (usedAbilities !== undefined)
+               {
+                  usedAbilities.push(ability.abilityKey);
+               }
+            }
+            else
+            {
+               myCallback();
+            }
+         }
+
+         LOGGER.trace("EventObserver.processAbilityQueue() end");
+      };
+
+      EventObserver.prototype.finishOnChange = function(store, callback)
+      {
+         LOGGER.trace("EventObserver.finishOnChange() start");
+
+         InputValidator.validateNotNull("store", store);
+         // callback optional.
+
+         store.dispatch(Action.clearEvent());
+
+         if (callback)
+         {
+            callback();
+         }
+
+         LOGGER.trace("EventObserver.finishOnChange() end");
+      };
 
       if (Object.freeze)
       {
